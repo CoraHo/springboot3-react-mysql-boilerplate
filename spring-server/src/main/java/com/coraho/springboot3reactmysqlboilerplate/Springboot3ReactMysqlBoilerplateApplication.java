21c238217eaf507d4e7427d39780379420aeae26
coraHo <cora9122@gmail.com>
2024-03-08 12:42:53 -0500

    Initialized client-server application.

---
+++ spring-server/src/main/java/com/coraho/springboot3reactmysqlboilerplate/Springboot3ReactMysqlBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.coraho.springboot3reactmysqlboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3ReactMysqlBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3ReactMysqlBoilerplateApplication.class, args);
+	}
+
+}
